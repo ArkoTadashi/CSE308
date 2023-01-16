18997011cc745ead98bf2a2943f86e464be25d63
ArkoTadashi <aurosikder@gmail.com>
2023-01-16 12:30:41 +0600

    RegularUSER done

--- Problem1/RegularUser.java
+++ Problem1/RegularUser.java
@@ -21,27 +21,48 @@ public class RegularUser extends User {
         prevState = abcServer.getPrevState();
         currState = abcServer.getCurrState();
 
-        if (prevState == State.OPERATIONAL && currState == State.PARTIALLY_DOWN) {
+        System.out.println("\n\t REGULAR USER ");
+
+        if (currState == State.OPERATIONAL) {
+            if (premium) {
+                System.out.println("You total due payment for using our premium services is $x");
+            }
+        }
+        else if (prevState == State.OPERATIONAL && currState == State.PARTIALLY_DOWN) {
+            System.out.println("Our server is updating \nIt is currently partially down");
             System.out.println("Using limited functionality");
-            System.out.println("Pay 20$ to use premium service? Type Yes or No.");
+            System.out.println("Pay $20 per hour to use premium service? \nType Yes or No");
             
+            String read = br.readLine();
+
+            if (read.equalsIgnoreCase("YES")) {
+                premium = true;
+                System.out.println("Thank you for subscribing to our premium services");
+                System.out.println("All of your data are copied to the backup server");
+            }
+            else {
+                System.out.println("You can always subscribe to our premium services to access your data at all times");
+            }
 
         }
         else if (prevState == State.OPERATIONAL && currState == State.FULLY_DOWN) {
+            System.out.println("Our server is updating and currently down");
+            System.out.println("Pay $20 per hour to use premium service? \nType Yes or No");
+            
+            String read = br.readLine();
 
+            if (read.equalsIgnoreCase("YES")) {
+                premium = true;
+                System.out.println("Thank you for subscribing to our premium services");
+                System.out.println("All of your data are copied to the backup server");
+                System.out.println("You can keep using the services");
+            }
+            else {
+                System.out.println("You can always subscribe to our premium services to access your data at all times");
+            }
         }
-        else if (prevState == State.PARTIALLY_DOWN && currState == State.OPERATIONAL) {
 
-        }
-        else if (prevState == State.PARTIALLY_DOWN && currState == State.FULLY_DOWN) {
-
-        }
-        else if (prevState == State.FULLY_DOWN && currState == State.OPERATIONAL) {
-
-        }
-        else if (prevState == State.FULLY_DOWN && currState == State.PARTIALLY_DOWN) {
-
-        }
+        System.out.println("\n\t __REGULAR USER__ ");
     }
 
 }
